37031755cf1fb22b31f2967c4b2d0f858b8cfcba
chuagnes <agnes.p.chu@gmail.com>
2017-05-31 16:14:56 -0700

    working redux

--- client/src/actionCreators.js
+++ client/src/actionCreators.js
@@ -1,38 +1,23 @@
 import axios from 'axios';
-// import { SET_USER_ID } from './actions';
 import $ from 'jquery';
 
-// console.log(SET_USER_ID, "setuserid")
-
 var setUserID = function (userid){
-  return { type: 'SET_USER_ID', userid: userid }
+  return { type: 'SET_USER_ID', userid }
 }
 
 var getUserInfo = function (){
   return dispatch => {
-    $.get('/getuserinfo')
-     .done((data) => {
-        console.log(data)
-        dispatch(setUserID(data))
-        // console.log(props);
-        // context.props.getUserID(data);
-        // this.setState({userid: data})
-     })
-     .fail((err)=> {
-        console.error(err)
-     })
+    axios
+      .get('/getuserinfo')
+      .then(response => {
+        dispatch(setUserID(response.data))
+      })
+      .catch(error => {
+        console.error('axios error', error)
+      });
     
   };
 }
 
 export default getUserInfo
 
-// axios
-//       .get('/getuserinfo')
-//       .then(response => {
-//         console.log(response.data, "RESPONSE DATA");
-//         dispatch(setUserID(response.data))
-//       })
-//       .catch(error => {
-//         console.error('axios error', error)
-//       });
