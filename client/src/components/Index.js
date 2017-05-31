f88cc8cc68eb94ca60291b623f4e474ce606bdf2
Tom Dickson <tom@tomdickson.eu  name = Tom Dickson>
2017-05-31 12:16:16 -0700

    Initial work on matching up tutors and students.

--- client/src/components/Index.js
+++ client/src/components/Index.js
@@ -3,11 +3,29 @@ import ReactDOM from 'react-dom';
 import { BrowserRouter, Route, Link } from 'react-router-dom'
 import AskQuestion from './AskQuestion.js';
 
-const Index = () => (
-  <div>
-    <h1> Questions </h1>    
-    <AskQuestion/>
-  </div>
-)
+class Index extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+    	id: 2
+    };
+	  
+	  var socket = io.connect();
+
+	  socket.on('connect', function () {
+	    socket.emit('hi, my userID is: ', this.state.id);
+	  });
+
+  }
+
+  render() {
+  	return (
+		  <div>
+		    <h2> Questions </h2>    
+		    <AskQuestion/>
+		  </div>
+		)
+  }
+}
 
 export default Index;
