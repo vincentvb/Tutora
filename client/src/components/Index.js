f9e7d5d85db1ee885a0d0888df003cac8f0bc23d
Tom Dickson <tom@tomdickson.eu  name = Tom Dickson>
2017-06-01 15:55:39 -0700

    Added simple alert modal.

--- client/src/components/Index.js
+++ client/src/components/Index.js
@@ -3,15 +3,28 @@ import ReactDOM from 'react-dom';
 import { BrowserRouter, Route, Link } from 'react-router-dom'
 import AskQuestion from './AskQuestion.js';
 import Classroom from './Classroom.js'
-
+import Dialog from 'material-ui/Dialog';
+import FlatButton from 'material-ui/FlatButton';
+import RaisedButton from 'material-ui/RaisedButton';
 
 class Index extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-    	id: 2
+    	id: 2,
+      open: false
     };
+    this.handleOpen = this.handleOpen.bind(this);
+    this.handleClose = this.handleClose.bind(this);
   }
+
+  handleOpen() {
+    this.setState({open: true});
+  };
+
+  handleClose() {
+    this.setState({open: false});
+  };
 
   componentWillMount() {
 	  // this.socket = io.connect();
@@ -36,11 +49,34 @@ class Index extends React.Component {
   }
 
   render() {
+    const actions = [
+      <FlatButton
+        label="Reject invitation"
+        primary={true}
+        onTouchTap={this.handleClose}
+      />,
+      <FlatButton
+        label="Take me to the classroom"
+        primary={true}
+        onTouchTap={this.handleClose}
+      />,
+    ];
+
   	return (
 		  <div>
 		    <h2> Questions </h2>
 		    <AskQuestion/>
         <Classroom/>
+        {/* Delete me (RaisedButton) when you hook up the Dialog Modal to render automatically */}
+        <RaisedButton label="Modal Dialog" onTouchTap={this.handleOpen} />
+        <Dialog
+          title="We found a tutor for your question!"
+          actions={actions}
+          modal={true}
+          open={this.state.open}
+        >
+          Accept and go to the classroom or wait.
+        </Dialog>
 		  </div>
 		)
   }
