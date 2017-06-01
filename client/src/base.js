b6ecd0d007d0559ae3299450cfa5884eb8d8e03f
Vincent Van Buskirk <vvanbuskirk805@gmail.com>
2017-06-01 16:13:46 -0700

    Fixed socket bugs

--- client/src/base.js
+++ client/src/base.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter, Route, Link } from 'react-router-dom'
+import { BrowserRouter, Route, Link, Switch } from 'react-router-dom'
 import injectTapEventPlugin from 'react-tap-event-plugin';
 import getMuiTheme from 'material-ui/styles/getMuiTheme';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import { Provider, connect } from 'react-redux'
+import Index from './components/Index.js';
+import Classroom from './components/Classroom.js'
+import AskQuestion from './components/AskQuestion.js';
+
 import store from './store';
 import App from './components/App.js'
 
@@ -14,11 +18,8 @@ injectTapEventPlugin();
 
 ReactDOM.render(
   <MuiThemeProvider muiTheme={getMuiTheme()}>
-    <BrowserRouter>
+
       <Provider store={store}>
         <App />
       </Provider>
-    </BrowserRouter>
   </MuiThemeProvider>, document.getElementById('root'));
-
-
--- client/src/components/Index.js
+++ client/src/components/Index.js
@@ -1,49 +1,66 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter, Route, Link } from 'react-router-dom'
+import { BrowserRouter, Route, Link, Redirect } from 'react-router-dom'
 import AskQuestion from './AskQuestion.js';
 import Classroom from './Classroom.js'
+import Nav from './Nav.js'
+import QuestionPage from '../containers/QuestionPage.js'
 
 
 class Index extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
+      redirect: false,
     	id: 2
     };
+    this.redirect = this.redirect.bind(this);
   }
 
   componentWillMount() {
-	  // this.socket = io.connect();
-    // console.log(this.socket);
-    //
-	  // this.socket.on('connect', (socket) => {
-	  //   this.socket.emit('userData', {
-    //     room: 'home',
-	  //   	message: 'User connected!',
-	  //   	user_id: this.state.id
-	  //   });
-	  // });
-    //
-    // this.socket.on('new message', (e) => {
-    //   console.log('message recieved');
-    //   this.recieveMessage(e);
-    // });
+	  this.socket = io.connect();
+    console.log(this.socket);
+
+	  this.socket.on('connect', (socket) => {
+	    this.socket.emit('userData', {
+        room: 'home',
+	    	message: 'User connected!',
+	    	user_id: this.state.id
+	    });
+	  });
+
+
   }
 
   componentWillUnmount() {
-    // this.socket.disconnect();
+    this.socket.disconnect();
+  }
+
+  redirect() {
+    this.setState({redirect: true})
   }
 
   render() {
+    if (this.state.redirect) {
+      console.log("IN HERE");
+      return (
+      <Redirect to = "/classroom" />
+      )
+    }
+    else {
   	return (
 		  <div>
+        <Nav />
+        <h1> Hello World </h1>
+        <QuestionPage />
 		    <h2> Questions </h2>
+        <p onClick={this.redirect}>Clickity click</p>
 		    <AskQuestion/>
-        <Classroom/>
 		  </div>
 		)
-  }
+
+   }
+ }
 }
 
 export default Index;
