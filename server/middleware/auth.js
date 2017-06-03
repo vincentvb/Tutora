3e58f7fc980233124424c0eaef4e8926d2630f74
chuagnes <agnes.p.chu@gmail.com>
2017-06-02 18:43:51 -0700

    added student tutor for fb login, fixed server rendering

--- server/middleware/auth.js
+++ server/middleware/auth.js
@@ -1,12 +1,23 @@
 const session = require('express-session');
 const RedisStore = require('connect-redis')(session);
 const redisClient = require('redis').createClient();
+const models = require('../../db/models');
 
 module.exports.verify = (req, res, next) => {
   if (req.isAuthenticated()) {
     return next();
   }
   res.redirect('/landingPage');
+};
+
+module.exports.update = (req, res, done) => {
+  return models.Profile.where({ email: req.user.email }).fetch()
+  .then(profile => {
+     profile.save({type: req.body.type}, {method: 'update'});
+  })
+  .error(err => {
+    console.error(err)
+  })
 };
 
 module.exports.session = session({
