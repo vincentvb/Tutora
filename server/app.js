d59d93c8690a269fe8a728d3596ab69dae4a7b61
Tom Dickson <tom@tomdickson.eu  name = Tom Dickson>
2017-06-14 16:29:55 -0700

    Work in progress with breaking out microservice. Maybe try to switch to http for server and for sockets to make it faster?

--- server/app.js
+++ server/app.js
@@ -51,89 +51,89 @@ var server = https.createServer(options, app).listen(3000, function(){
   console.log("listening on 3000");
 });
 
-const io = require('socket.io')(server);
+// const io = require('socket.io')(server);
 
-io.on('connection', (socket) => {
-  socket.on('userData', (data) => {
-    socket.join(data.room);
-    socket.userId = data.user_id
-    socket.room = data.room;
-    client.set(socket.userId, "online");
-    client.sadd("online", socket.userId);
-    io.to('home').emit('newonlineuser', socket.userId );
-
-
-  })
-
-  socket.on('roomJoin', (data) => {
-    // console.log(data);
-    socket.userId = data.user_id
-    socket.join(data.room);
-    socket.room = data.room
-})
-
-  socket.on('connectionRequest', (data) => {
-    io.to('home').emit('alertMessage', {
-      receivingUser: data.receivingUser,
-      requestUser: data.requestUser,
-      roomName: data.roomName,
-      questionId: data.questionId
-    })
-  })
-
-  // when a student has posted a question
-  socket.on('postedQ', question => {
-    io.to('home').emit('postedQ', question);
-  })
+// io.on('connection', (socket) => {
+//   socket.on('userData', (data) => {
+//     socket.join(data.room);
+//     socket.userId = data.user_id
+//     socket.room = data.room;
+//     client.set(socket.userId, "online");
+//     client.sadd("online", socket.userId);
+//     io.to('home').emit('newonlineuser', socket.userId );
 
 
-  //  maybe refactor to a reverse index 
-  socket.on('checkOnline', (user) => {
-    console.log(user, "CHECK ONLINE QUESTION")
+//   })
 
-    Promise.map(user.questions, (question) => {
-      return client.getAsync(question.profile_id)
-      .then(res => {
-        if (res === "online") {
-          return question
-        }
-      })
-    })
-    .then(questions => {
-      // console.log(questions, "IN CHECK ONLINE");
-      questions = questions.filter(function(question){
-        return question !== undefined;
-      });
+//   socket.on('roomJoin', (data) => {
+//     // console.log(data);
+//     socket.userId = data.user_id
+//     socket.join(data.room);
+//     socket.room = data.room
+// })
 
-      console.log(questions, "I FOUND THE ONLINE QUESTIONS")
-      io.to('home').emit('onlineQs', questions )
-    })
-    .catch(err => console.log(err))
+//   socket.on('connectionRequest', (data) => {
+//     io.to('home').emit('alertMessage', {
+//       receivingUser: data.receivingUser,
+//       requestUser: data.requestUser,
+//       roomName: data.roomName,
+//       questionId: data.questionId
+//     })
+//   })
 
-  })  
+//   // when a student has posted a question
+//   socket.on('postedQ', question => {
+//     io.to('home').emit('postedQ', question);
+//   })
 
-  socket.on('chatMessage', (data) => {
-    io.to(data.room).emit('newMessage', data.message);
-  })
 
-  socket.on('disconnect', () => {
-    if (socket.userId) {
-      client.set(socket.userId, "offline")
-      setTimeout(() => {
-        client.get(socket.userId, (err, reply) => {
-          if (reply === "offline") {
-            client.del(socket.userId)
-            client.srem("online", socket.userId)
-            io.to('home').emit('delete')
-            io.to('home').emit('re-renderQs')
-            console.log("somebody logged off");
-          }
-        })
-      }, 1000)
-     }
-  })
-  console.log('a user connected');
-})
+//   //  maybe refactor to a reverse index 
+//   socket.on('checkOnline', (user) => {
+//     console.log(user, "CHECK ONLINE QUESTION")
+
+//     Promise.map(user.questions, (question) => {
+//       return client.getAsync(question.profile_id)
+//       .then(res => {
+//         if (res === "online") {
+//           return question
+//         }
+//       })
+//     })
+//     .then(questions => {
+//       // console.log(questions, "IN CHECK ONLINE");
+//       questions = questions.filter(function(question){
+//         return question !== undefined;
+//       });
+
+//       console.log(questions, "I FOUND THE ONLINE QUESTIONS")
+//       io.to('home').emit('onlineQs', questions )
+//     })
+//     .catch(err => console.log(err))
+
+//   })  
+
+//   socket.on('chatMessage', (data) => {
+//     io.to(data.room).emit('newMessage', data.message);
+//   })
+
+//   socket.on('disconnect', () => {
+//     if (socket.userId) {
+//       client.set(socket.userId, "offline")
+//       setTimeout(() => {
+//         client.get(socket.userId, (err, reply) => {
+//           if (reply === "offline") {
+//             client.del(socket.userId)
+//             client.srem("online", socket.userId)
+//             io.to('home').emit('delete')
+//             io.to('home').emit('re-renderQs')
+//             console.log("somebody logged off");
+//           }
+//         })
+//       }, 1000)
+//      }
+//   })
+//   console.log('a user connected');
+// })
 
 module.exports = app;
 module.exports = client;
